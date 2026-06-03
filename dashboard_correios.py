f6ac41f507b7b3965e81312f59cc55c77157553f
Felipe-amazon-teo <feltteod@amazon.com>
2026-06-02 21:19:07 -0300

    Update dashboard_correios.py

--- dashboard_correios.py
+++ dashboard_correios.py
@@ -53,7 +53,7 @@ st.markdown("""
     .alert-ruptura {
         background-color: #6f42c1;
         color: white;
-        padding: 0.3rem;
+        padding: 0.1rem;
         border-radius: 8px;
         text-align: center;
         font-size: 1.1rem;
