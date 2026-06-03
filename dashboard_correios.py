78dfe02479ee736b3e46950ef9406a5ee480680e
Felipe-amazon-teo <feltteod@amazon.com>
2026-06-02 21:10:06 -0300

    Update dashboard_correios.py

--- dashboard_correios.py
+++ dashboard_correios.py
@@ -66,7 +66,7 @@ st.markdown("""
         100% { opacity: 1; transform: scale(1); }
     }
     .sirene {
-        font-size: 2.0rem;
+        font-size: 1.8rem;
         animation: rotate 0.5s infinite;
     }
     @keyframes rotate {
